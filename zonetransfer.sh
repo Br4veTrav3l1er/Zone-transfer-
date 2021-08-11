e803eb0e718a5b8d3d42037a585e73f324d85e26
paradoxite <42909126+paradoxite@users.noreply.github.com>
2021-08-11 16:45:30 +0200

    Create zonetransfer.sh

---
+++ zonetransfer.sh
@@ -0,0 +1,17 @@
+  GNU nano 5.4                                                                zonetransfer.sh                                                                          
+#!/bin/bash
+
+if [ "$1" == " " ]
+then
+echo "Usage: ztransferscanner.sh <domain>"
+else
+domain="$1"
+name_servers=$(host -t ns $domain | cut -d " " -f 4 | sed 's/.$//')
+for i in ${name_servers[@]}
+do
+dig axfr @"${i}" "$domain"
+echo "${i}"
+done
+fi
+
+
